6b549714c169469117521c98197b8d0ce5cf780e
connorhs <ch72@hw.ac.uk>
2022-02-02 20:32:16 +0000

    Added comments
    Added comments for readability

--- Assignment1/Assignment1.ino
+++ Assignment1/Assignment1.ino
@@ -3,8 +3,10 @@
 #define switch1 16
 #define switch2 17
 
+// A variable used to indicate whether the outputs will be connected to LEDs or the oscilloscope
 unsigned char usingLEDs = 1;
 
+// Variable declaration for pulse stream parameters
 int widthA;
 int initialWidthA;
 int widthChangeA;
@@ -15,11 +17,11 @@ int pulseCounterMax = 17;
 unsigned char disablePulseStream = 0;
 unsigned char modifiedMode = 0;
 
+// Interrupt service routines for switches 1 & 2. On call, toggle the desired variable
 void IRAM_ATTR ISR_switch1() {
   if (disablePulseStream == 1) { disablePulseStream = 0; }
   else { disablePulseStream = 1; }
 }
-
 void IRAM_ATTR ISR_switch2() {
   if (modifiedMode == 1) { modifiedMode = 0; }
   else { modifiedMode = 1; }
@@ -29,14 +31,18 @@ void setup() {
 
   Serial.begin(9600);
 
+  // Set up pins 0 & 4 as outputs
   pinMode(pulseA, OUTPUT);
   pinMode(pulseB, OUTPUT);
 
+  // Set up pins 16 & 17 as inputs with internal pullup resistors
   pinMode(switch1, INPUT_PULLUP);
   pinMode(switch2, INPUT_PULLUP);
+  // Attach interrupts to the switches on the rising edge
   attachInterrupt(switch1, ISR_switch1, RISING);
   attachInterrupt(switch2, ISR_switch2, RISING);
 
+  // Increase the length of pulse stream parameters if using LEDs
   if (usingLEDs == 1)
   {
     initialWidthA = 800000;
@@ -57,31 +63,39 @@ void setup() {
 
 void loop() 
 {  
+  // If modifiedMode (toggled by switch2) is high, C = C-3
   if (modifiedMode == 1) {pulseCounterMax = 14; }
   else { pulseCounterMax = 17; }
 
   Serial.print("\n\ndisablePulseStream = "); Serial.print(disablePulseStream);
   Serial.print("\nmodifiedMode = "); Serial.print(modifiedMode);
   Serial.print("\npulseCounterMax = "); Serial.print(pulseCounterMax);
-  
+
+  // start the pulse stream only if not disabled by disablePulseStream(switch1)
   if (!disablePulseStream)
   {
+    // Reset the width of the signal A pulse
     widthA = initialWidthA;
-    
+
+    // Pulse signal B for widthB microseconds
     digitalWrite(pulseB, HIGH);
     delayMicroseconds(widthB);
     digitalWrite(pulseB, LOW);
 
     Serial.print("\npulseWidthA = "); 
+    // Loop through all pulses in signal A, length depending on the status of modifiedMode(switch2)
     for (int pulseCounter = 0; pulseCounter < pulseCounterMax; pulseCounter++)
     {
       Serial.print(widthA); Serial.print("\t");
+      // Pulse signal A for widthA microseconds followed by a space of spaceA microseconds
       digitalWrite(pulseA, HIGH);
       delayMicroseconds(widthA);
       digitalWrite(pulseA, LOW);
       delayMicroseconds(spaceA);
+      // Increment the width of the signal A pulse for the next loop
       widthA += widthChangeA;   
     }
+    // Leave a space of spaceD microseconds
     delayMicroseconds(spaceD);
   }
 }
